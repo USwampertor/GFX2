96c5f9dc275089bf9e5aa5928da1811829c94f11
Marco <idv16c.mmillan@uartesdigitales.edu.mx>
2018-05-17 18:43:48 -0500

    +added project

---
+++ Graficas2_1/Graphics_API.h
@@ -0,0 +1,151 @@
+#pragma once
+#include <d3d11.h>
+#include <vector>
+#include <exception>
+
+
+class GraphicsBuffer
+{
+public:
+///We gonna make a virtual destructor because we will create derivate clases from this one, and so each class will provide its own destructor
+	GraphicsBuffer() = default;
+	virtual ~GraphicsBuffer()
+	{
+		if (m_pBuffer)
+		{
+			m_pBuffer->Release();
+		}
+	}
+///
+	ID3D11Buffer* m_pBuffer = nullptr;
+
+	//VertexBuffer
+	//IndexBuffer
+};
+
+
+template<typename TVERTEX>
+class VertexBuffer final : public GraphicsBuffer
+{
+public:
+	VertexBuffer() = default;
+	~VertexBuffer()
+	{
+		Clear();
+	}
+
+	void Reserve(size_t numObjects)
+	{
+		m_vertexData.reserve(numObjects);
+	}
+
+	void Add(const TVERTEX& vertex)
+	{
+		m_vertexData.push_back(vertex);
+	}
+	void Add(std::vector<TVERTEX>& vertices)
+	{
+		m_vertexData.insert(m_vertexData.end(), vertices.begin(),vertices.end());
+	}
+	void Add(TVERTEX* pVertices, size_t numVer)
+	{
+		m_vertexData.insert(m_vertexData.end(),pVertices,pVertices+numVer);
+	}
+	void Clear()
+	{
+		m_vertexData.clear();
+	}
+	void CreateHardWareBuffer(ID3D11Device* pd3dDevice, unsigned int usage = D3D11_USAGE_DEFAULT)
+	{
+		D3D11_BUFFER_DESC bd;
+		memset(&bd, 0, sizeof(bd));
+
+		bd.Usage = usage;
+		bd.ByteWidth = static_cast<UINT32>(sizeof(TVERTEX)*m_vertexData.size());
+		bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		bd.CPUAccessFlags = 0;
+
+		D3D11_SUBRESOURCE_DATA InitData;
+		memset(&InitData, 0, sizeof(InitData));
+		InitData.pSysMem = &m_vertexData[0];
+
+		HRESULT hr = pd3dDevice->CreateBuffer(&bd, &InitData, &m_pBuffer);
+		if (FAILED(hr))
+		{
+			throw std::exception("Failed to create Vertex Buffer you idiot");
+		}
+	}
+	
+private:
+	std::vector<TVERTEX> m_vertexData;
+};
+
+template <typename TINDEX>
+
+class IndexBuffer final : public GraphicsBuffer
+{
+public:
+	IndexBuffer() = default;
+	~IndexBuffer()
+	{
+		Clear();
+	}
+
+	void Reserve(size_t numObjects)
+	{
+		m_indexData.reserve(numObjects);
+	}
+
+	void Add(const TINDEX& index)
+	{
+		m_indexData.push_back(index);
+	}
+	void Add(std::vector<TINDEX>& indices)
+	{
+		m_indexData.insert(m_indexData.end(), indices.begin(), indices.end());
+	}
+	void Add(TINDEX* pIndices, size_t numInd)
+	{
+		m_indexData.insert(m_indexData.end(), pIndices, pIndices + numInd);
+	}
+	void Clear()
+	{
+		m_indexData.clear();
+	}
+	void CreateHardWareBuffer(ID3D11Device* pd3dDevice, unsigned int usage = D3D11_USAGE_DEFAULT)
+	{
+		D3D11_BUFFER_DESC bd;
+		memset(&bd, 0, sizeof(bd));
+
+		bd.Usage = usage;
+		bd.ByteWidth = static_cast<UINT32>(sizeof(TINDEX)*m_indexData.size());
+		bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		bd.CPUAccessFlags = 0;
+
+		D3D11_SUBRESOURCE_DATA InitData;
+		memset(&InitData, 0, sizeof(InitData));
+		InitData.pSysMem = &m_indexData[0];
+
+		HRESULT hr = pd3dDevice->CreateBuffer(&bd, &InitData, &m_pBuffer);
+		if (FAILED(hr))
+		{
+			throw std::exception("Failed to create Index Buffer you idiot");
+		}
+	}
+
+	void Write(ID3D11DeviceContext* pDeviceContext , void* pData, size_t numBytes)
+	{
+		D3D11_MAPPED_SUBRESOURCE subRes;
+		memset(&subRes, 0, sizeof(subRes));
+		//from this buffer we wanna do something
+		pDeviceContext->Map(m_pBuffer, 0, nullptr, D3D11_MAP_DISCARD, &subRes);
+		//here we write all the shit we need, the thing is that this is not relative to anything specific
+
+
+		//we later discard the original thing because we dont need it anymore
+		pDeviceContext->Unmap(m_pBuffer, 0);
+	}
+private:
+	std::vector<TINDEX> m_indexData;
+};
+
